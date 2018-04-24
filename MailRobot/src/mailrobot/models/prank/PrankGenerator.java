c701d4d87b11b03f2a402753121934c5b09ff6b1
olliens <oussama.lagha@heig-vd.ch>
2018-04-24 17:43:33 +0200

    fix Prank generator

--- MailRobot/src/mailrobot/models/prank/PrankGenerator.java
+++ MailRobot/src/mailrobot/models/prank/PrankGenerator.java
@@ -5,10 +5,52 @@
  */
 package mailrobot.models.prank;
 
+import java.io.IOException;
+import mailrobot.config.ConfigManager;
+import java.util.LinkedList;
+import java.util.Random;
+
 /**
  *
  * @author Lagha Oussama - Jobin Simon
  */
 public class PrankGenerator {
     
+    private ConfigManager cf;
+    
+    private LinkedList<Prank> pranks;
+    
+    public PrankGenerator() throws IOException {
+        cf = new ConfigManager();
+        pranks = new LinkedList();
+        
+        for(int i = 0; i < cf.getNombreGroupe(); ++i) {
+            pranks.add(new Prank());
+        }
+    }
+    
+    public void generatePrank() {
+       
+        Random rand = new Random();
+        
+        int nbVictims = cf.getVictims().size();
+        
+        for(Prank prank : pranks) {
+            
+            prank.addWitnessRecipients(cf.getWitness());
+            int idVictimSender = rand.nextInt(nbVictims);
+            prank.setVictimSender(cf.getVictims().get(idVictimSender));
+            
+            LinkedList randomVictims = new LinkedList();
+            for(int i = 0; i < nbVictims / cf.getNombreGroupe(); ++i) {
+                
+                int idVictim = rand.nextInt(nbVictims);
+                while(idVictim == idVictimSender) {
+                    idVictim = rand.nextInt(nbVictims);                 
+                }
+                randomVictims.add(cf.getVictims().get(rand.nextInt(nbVictims)));
+            }
+            prank.addVictimRecipients(randomVictims);
+        }
+    }
 }
